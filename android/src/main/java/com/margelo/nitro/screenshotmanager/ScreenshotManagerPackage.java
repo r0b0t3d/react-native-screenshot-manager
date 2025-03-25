fa237ea2821439418c17ec76efc1728a12d3baa7
Tuan Luong <tuanluong.it@gmail.com>
2025-03-25 20:56:59 +0700

    build lib

--- android/src/main/java/com/margelo/nitro/screenshotmanager/ScreenshotManagerPackage.java
+++ android/src/main/java/com/margelo/nitro/screenshotmanager/ScreenshotManagerPackage.java
@@ -1,17 +1,13 @@
 package com.margelo.nitro.screenshotmanager;
 
-import android.util.Log;
-
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
-import com.margelo.nitro.core.HybridObject;
 
 import java.util.HashMap;
-import java.util.function.Supplier;
 
 public class ScreenshotManagerPackage extends TurboReactPackage {
   @Nullable
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-// TODO: Export all HybridObjects here for the user
+import { NitroModules } from 'react-native-nitro-modules';
+import type { ScreenshotManager } from './specs/ScreenshotManager.nitro';
+
+const ScreenshotManagerHybridObject =
+  NitroModules.createHybridObject<ScreenshotManager>('ScreenshotManager');
+
+export function enabled(value: boolean) {
+  ScreenshotManagerHybridObject.enabled(value);
+}
+export function enableSecureView(imagePath?: string) {
+  ScreenshotManagerHybridObject.enableSecureView(imagePath);
+}
+export function disableSecureView() {
+  ScreenshotManagerHybridObject.disableSecureView();
+}
+export function usePreventScreenshot() {}
+export function useDisableSecureView() {}
+export function addListener(fn: () => void): () => void {
+  return ScreenshotManagerHybridObject.addListener(fn);
+}
